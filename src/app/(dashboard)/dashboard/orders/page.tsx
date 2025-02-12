d4391e7992f7511368bf0e9718ed8dadb980cfc1
sadequlfakir <sadikul0140@gmail.com>
2025-02-12 17:11:55 +0600

    Updated orders page

--- src/app/(dashboard)/dashboard/orders/page.tsx
+++ src/app/(dashboard)/dashboard/orders/page.tsx
@@ -433,7 +433,7 @@ const Row = ({
                     <Button
                       onClick={() => {
                         setOpen(false);
-                        setPayOpen(true);
+                        setIsPayMethodOpen(true);
                       }}
                       className="sm:w-[115px] py-0 h-8 sm:h-9 !text-xs sm:!text-sm font-semibold flex items-center justify-center gap-x-2"
                     >
